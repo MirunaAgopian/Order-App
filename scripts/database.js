722d9f1b9301b0dd0831838418b9ca8a6c1c1290
Miruna Agopian <agopian.miruna@yahoo.com>
2025-07-22 18:27:32 +0200

    the whole project file was added

---
+++ scripts/database.js
@@ -0,0 +1,79 @@
+let dishes = {
+    "mainDishes": [
+    {
+        "name": "Pizza Meeresfrüchte", 
+        "description": "Mit Garnellen und Kapern",
+        "price": 12.50,
+        "dishAmount": 1
+    },
+    {
+        "name": "Schweinesteak", 
+        "description": "Mit Spargel und Kräuterbutter",
+        "price": 16.50,
+        "dishAmount": 1
+    },
+    {
+        "name": "Haus Beefburger", 
+        "description": "Mit Scharfsoße und Pommes",
+        "price": 13.50,
+        "dishAmount": 1
+    },
+    {
+        "name": "Sommersalat", 
+        "description": "Mit Fetakäse, Oliven und Honig-Senf-Soße",
+        "price": 10.50,
+        "dishAmount": 1
+    },
+    {
+        "name": "Grillgemüse", 
+        "description": "Mit Hummus und hausgemachtes Fladenbrot",
+        "price": 8.50,
+        "dishAmount": 1
+    },
+    {
+        "name": "Tomatensuppe", 
+        "description": "Mit Basilikomsoße und Pizzabrot",
+        "price": 9.50,
+        "dishAmount": 1
+    }
+    ],
+
+    "sideDishes": [
+         {
+        "name": "Spargel vom Grill", 
+        "description": "Mit Parmesan und Bauchspeck",
+        "price": 5.50,
+        "dishAmount": 1
+    },
+     {
+        "name": "Kartoffelknödel", 
+        "description": "Mit Petersilie",
+        "price": 6.50,
+        "dishAmount": 1
+    },
+    {
+        "name": "Pommes", 
+        "description": "Mit Mayo und Ketchup",
+        "price": 5.50,
+        "dishAmount": 1
+    }
+    ],
+    "desserts": [
+         {
+        "name": "Kaiserschmarrn", 
+        "description": "Mit Pflaumenkompott und Apfelmus",
+        "price": 12.50,
+        "dishAmount": 1
+    },
+     {
+        "name": "Spaghettieis", 
+        "description": "Mit Erdbeeren, Sahne und Vanillesoße",
+        "price": 10.50,
+        "dishAmount": 1
+    }
+    ]
+
+};
+
+let basketDishes = [];
+
